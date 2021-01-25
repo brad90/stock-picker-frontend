d585e6c45c49ba71ea353b3926720ffd30c2f31d
Stephen <stephen@MacBook-Air.local>
2021-01-25 10:26:46 +0000

    First commit

---
+++ src/components/navigations/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./header.css"
+
+class Header extends Component {
+  render() {
+    return (
+      <nav className="navigation">
+        <p>Home</p>
+        <p>Home</p>
+      </nav>
+
+    );
+  }
+}
+
+export default Header;
