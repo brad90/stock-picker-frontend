068b461771ffb40c1eba85e5556062529ffe4f28
Stephen <stephen@MacBook-Air.local>
2021-03-23 11:57:20 +0000

    Pass search term to backend

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,47 +1,72 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import STOCKS from "../common/ticker_master.js"
+import API_URL from "../common/apis"
+import axios from "axios";
+
 
 
 
 const Home = () => {
 
- const [stocks, setStocks] = useState([])
- const [selectedStock, setSelectedStock] = useState("")
-
- const onSelectAuto = (stock) => {
-   setSelectedStock(stock)
- }
-
- const autoSuggestStocks = stocks.map((stock) =>
-  <li key={stock} onClick={() => onSelectAuto(stock)}>
-   {stock}
-  </li>
- )
-
- const onInputSearchStocks = (search) => {
+  const [filteredSearch, setFilteredSearch] = useState([])
+  const [selectedSearch, setSelectedSearch] = useState("")
   
-  setSelectedStock(search)
+  useEffect(() => {
+    document.addEventListener("keyup", event => {
+      if (event === 13) {
+          makeSearch()
+        }
+    })
+  })
 
-  if (search != "") {
-   const filteredStocks = STOCKS.filter(stock =>
-   stock.includes(search.toUpperCase()))
-   setStocks(filteredStocks)
-  } else {
-   setStocks([])
+
+  const onSelectAuto = (term) => {
+    setSelectedSearch(term)
+    setFilteredSearch([])
   }
 
+
+  const onInputSearch = (search) => {
+    setSelectedSearch(search)
+    if (search !== "") {
+      const filteredStocks = STOCKS.filter(stock =>
+      stock.includes(search.toUpperCase()))
+      setFilteredSearch(filteredStocks)
+    } else {
+      setFilteredSearch([])
+    }
+  }
   
- }
+  const makeSearch = () => {
+
+    axios.post(API_URL['browse'] + `search/${selectedSearch}`, {
+      search: selectedSearch
+    })
+      .then((res) => {
+      console.log(res)
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+
+
+  const autoSuggestedStocks = filteredSearch.map((term) =>
+    <li key={term} onClick={() => onSelectAuto(term)}>
+    {term}
+    </li>
+  )
  
 
  return (
   <span>
-   <h1>Check if its the right time to buy your stock?</h1>
-   <input type="text" name="stockSearch" value={selectedStock}style={{textTransform:"uppercase"}} onChange={e => onInputSearchStocks(e.target.value)}/>
-   <ul>
-    {autoSuggestStocks}
-   </ul>
+     <h1>Check if its the right time to buy your stock?</h1>
+      <input type="text" name="stockSearch" value={selectedSearch} style={{ textTransform: "uppercase" }} onChange={e => onInputSearch(e.target.value)} />
+      <button onClick={() => makeSearch()}> Search </button>
+  <ul>
+    {autoSuggestedStocks}
+  </ul>
+
   </span>
  )
 }
